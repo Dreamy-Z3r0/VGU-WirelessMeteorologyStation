043f1255480657c28282916fe69dd97de5752cef
Dreamy-Z3r0 <danielblake98vt@gmail.com>
2022-07-17 17:49:00 +0700

    update one of anemometer reading methods

--- 01 - Module Testings/06-Anemometer/Anemometer_FixedDuration/Anemometer_FixedDuration.ino
+++ 01 - Module Testings/06-Anemometer/Anemometer_FixedDuration/Anemometer_FixedDuration.ino
@@ -12,10 +12,11 @@
  ***                                                               ***
  *** Anemometer_FixedDuration tests the first method. The fixed    ***
  *** duration is pre-defined at the very beginning of the program. ***
- *** Sampling routine is called every minute, and the counter is   ***
- *** incremented whenever the switch (anemometer's reed switch)    ***
- *** toggles, which gives 2 edges per revolution instead of 1. The ***                                                            
- *** wind speed is then calculated as followed:                    ***                                                            
+ *** Sampling routine is called after a fixed period from the last ***
+ *** one, and the counter is incremented whenever the switch       ***
+ *** (anemometer's reed switch) toggles, which gives 2 edges per   ***
+ *** revolution instead of 1. The wind speed is then calculated as ***
+ *** followed:                                                     ***                                                            
  ***    windSpeed = (inputCounter / (2 * duration)) * (2/3) (m/s)  ***                                                            
  ***              =  inputCounter / (3 * duration)          (m/s)  ***    
  ***                                                               ***                                        
@@ -25,71 +26,116 @@
  ***                                                               ***    
  *** Note 2: Any external interrupt pin could be used as input pin ***                                                            
  ***         for the anemometer.                                   ***                                                               
+ ***                                                               ***        
+ *** Note 3: This code utilizes the technique of taking mean value ***                                                          
+ ***         by reading a fixed number of n-second wind speed data ***
+ ***         continuously over 1 sampling period.                  ***
+ ***                                                               ***
  *********************************************************************
  *********************************************************************/
 
 
 #define input_Anemometer PB2
-unsigned int inputCounter = 0;
+#define timeBetweenReadingPeriods 5  // minutes
+unsigned long idleTimeInMilliseconds;
 
-#define duration 10
-uint8_t Tracker_duration = 0;
+#define rawSampleQuantity 20    // number of sampling slots per sensor reading period
+#define sampleDuration 5        // seconds per sample
+uint8_t secondCounter = 0;
 
-uint8_t minuteTracker = 0;
-bool newMinute = true;
+unsigned int rawData[rawSampleQuantity];
+uint8_t rawData_index = 0;
+bool rawData_ready = false;
+
+double windSpeed;
+bool sensorReadingRequested = false;
+bool idlePeriod = false;
+
+TIM_TypeDef *Instance = TIM1;
+HardwareTimer *InputRoutineTim = new HardwareTimer(Instance);
+
+unsigned long timestamp;
 
 void setup() {
   Serial.begin(9600);
+  idleTimeInMilliseconds = timeBetweenReadingPeriods * 60000;
 
-  TIM_TypeDef *Instance = TIM1;
-  HardwareTimer *InputRoutineTim = new HardwareTimer(Instance);
+  pinMode(input_Anemometer, INPUT);
 
   InputRoutineTim->setOverflow(1, HERTZ_FORMAT);
-  InputRoutineTim->attachInterrupt(std::bind(secondCounter, &minuteTracker));
-  InputRoutineTim->resume();
+  InputRoutineTim->attachInterrupt(samplingTimeSlot);
+
+  sensorReadingRequested = true;
 }
 
 void loop() {
-  if (newMinute) {
-    newMinute = false;
-    RunEveryMinute();
+  if (idlePeriod) {
+    if (timestamp > millis()) {   // request a new wind speed update in case timer for millis() overflows
+      idlePeriod = false;
+      sensorReadingRequested = true;
+    }
+    else if ((millis() - timestamp) >= idleTimeInMilliseconds) {  // request a new wind speed update after [timeBetweenReadingPeriods]-minute idle period
+      idlePeriod = false;
+      sensorReadingRequested = true;
+    }
+  }
+  
+  if (sensorReadingRequested) {
+    sensorReadingRequested = false;  // clear request
+    
+    rawData[rawData_index] = 0;      // reset raw data at array index 0
+    InputRoutineTim->setCount(0);    // reset timer counter
+    secondCounter = 0;               // make sure second counter starts at 0
+    
+    InputRoutineTim->resume();       // start a new sensor reading period
+    attachInterrupt(digitalPinToInterrupt(input_Anemometer), anemometerEdgeDetected, CHANGE);
+  }
+
+  if (rawData_ready) {
+    idlePeriod = true;
+    timestamp = millis();
+
+    rawData_ready = false;
+    windSpeed_update();
+
+    Serial.print("Wind speed: ");
+    Serial.print(windSpeed, 4);
+    Serial.println(" m/s");
   }
 }
 
-void secondCounter(uint8_t* counter) {
-  *counter += 1;
-  if (60 == *counter) 
-  {
-    *counter = 0;
-    newMinute = true;
+void anemometerEdgeDetected(void) {
+  rawData[rawData_index] += 1;
+}
+
+void samplingTimeSlot() {     // time the [sampleDuration] second slot of 1 raw data
+  secondCounter += 1;
+  
+  if (sampleDuration == secondCounter) {    // a sampling time slot is up
+    InputRoutineTim->pause();       // pause timer
+    detachInterrupt(digitalPinToInterrupt(input_Anemometer));   // stop taking inputs
+    
+    InputRoutineTim->setCount(0);   // reset timer counter
+    
+    secondCounter = 0;      // reset second counter
+    rawData_index += 1;     // increment raw data array index
+    
+    if (rawSampleQuantity == rawData_index) {   // raw data array is full 
+      rawData_index = 0;      // reset array index
+      rawData_ready = true;   // indicates raw data array is full
+    }
+    else {
+      rawData[rawData_index] = 0;   // reset raw data holder slot
+      InputRoutineTim->resume();    // resume timer counter
+      attachInterrupt(digitalPinToInterrupt(input_Anemometer), anemometerEdgeDetected, CHANGE);   // resume sampling raw data
+    }
   }
 }
 
-void RunEveryMinute() {
-  inputCounter = 0;
-  Tracker_duration = 0;
-
-  TIM_TypeDef *Instance = TIM2;
-  HardwareTimer *InputTim = new HardwareTimer(Instance);
-
-  InputTim->setOverflow(1, HERTZ_FORMAT);
-  InputTim->attachInterrupt(std::bind(secondCounter, &Tracker_duration));
-  InputTim->setCount(0);
-  
-  attachInterrupt(digitalPinToInterrupt(input_Anemometer), readAnemometer, CHANGE);
-  InputTim->resume();
-  
-  while (Tracker_duration < duration);
-  detachInterrupt(digitalPinToInterrupt(input_Anemometer));
-  InputTim->pause();
-  delete InputTim;
-
-  float windSpeed = inputCounter / (duration * 3.0);
-  Serial.print("Wind speed: ");
-  Serial.print(windSpeed, 3);
-  Serial.println(" m/s");
-}
-
-void readAnemometer() {
-  inputCounter += 1;
+void windSpeed_update(void) {   // update the wind speed value after the total sampling period of [rawSampleQuantity] * [sampleDuration] seconds is over
+  windSpeed = 0;
+  for (uint8_t i = 0; i < rawSampleQuantity; i += 1) {    // take the sum of [sampleDuration]-second wind speed values from raw data
+    windSpeed += (rawData[i] / (3.0 * sampleDuration));
+  }
+  windSpeed /= rawSampleQuantity;   // take the mean value of wind speed over the [rawSampleQuantity] samples
 }
