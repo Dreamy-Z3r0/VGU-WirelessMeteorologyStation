afd9778a4247a91f71513165a7b11b5910dfb1cb
Dreamy-Z3r0 <danielblake98vt@gmail.com>
2022-12-12 16:08:03 +0700

    add descriptions

--- 01 - Module Testings/08-Rain-Gauge/RainGauge_SimpleTest/RainGauge_SimpleTest.ino
+++ 01 - Module Testings/08-Rain-Gauge/RainGauge_SimpleTest/RainGauge_SimpleTest.ino
@@ -1,3 +1,29 @@
+/******************************************************************************
+ ******************************************************************************
+ ***   Rain gauge interfacing test for STM32F103CBT6 on Arduino framework   ***
+ *** ---------------------------------------------------------------------- ***
+ ***                                                                        ***
+ *** The rain gauge in use is a self-emptying tipping bucket that gives of  ***
+ *** a pulse for every 0.2794 mm of rainfall.                               ***
+ ***                                                                        ***
+ *** This scope aims at testing interfacing the rain gauge and the micro-   ***
+ *** controller. Moreover, it is used to help determine the necessary low-  ***
+ *** pass filter which denoises signals from the sensor.                    ***
+ ***                                                                        ***
+ *** Setup:                                                                 ***
+ ***   (1) Tipping bucket --> Microcontroller                               ***
+ ***                      |-> Logic analyzer                                ***
+ ***                      |-> Oscilloscope                                  ***
+ ***                                                                        ***
+ ***   (2) Tipping bucket --> LPF --> Microcontroller                       ***
+ ***                              |-> Logic analyzer                        ***
+ ***                              |-> Oscilloscope                          ***
+ ***                                                                        ***
+ *** Note: Logic analyzer is used to validate the resolution of the rain    ***
+ ***       gauge (1 pulse for 0.2794mm of precipitation).                   ***
+ ******************************************************************************
+ ******************************************************************************/
+
 #define rainGaugeInput PB3
 #define rainfallPerTip 0.2794
 
